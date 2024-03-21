6fbb2e2c35315b78ba4725eafd1d4c4f54a1ca02
mirinaehi <garook@e-mirim.hs.kr>
2024-03-21 11:57:00 +0900

    02-01 이름공간

--- 2024-CPP-202/grammar/02-01 기본예제.cpp
+++ 2024-CPP-202/grammar/02-01 기본예제.cpp
@@ -1,8 +1,11 @@
 ﻿#include <iostream>
 
+// 이름공간. std::를 안해도 된다
+using namespace std;
+
 int main(void)
 {
 	// cout: 출력객체, endl: 개행객체
-	std::cout << "Hello World"<< std::endl;
+	cout << "Hello World"<< endl;
 	return 0;
 }

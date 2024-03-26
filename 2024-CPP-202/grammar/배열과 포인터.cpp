cd6ef7748803864374633b0efc6c122b612b76ed
mirinaehi <garook@e-mirim.hs.kr>
2024-03-26 16:03:07 +0900

    배열의 주소값

---
+++ 2024-CPP-202/grammar/배열과 포인터.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+
+int main(void)
+{
+	int arr[4] = { 10, 20, 30 ,40 };
+	char str[4] = "abc";
+
+	// 주소값 4바이트 만큼의 차이
+	printf("%d %d \n", &arr[0], &arr[1]);
+	// 주소값 1바이트 만큼의 차이
+	printf("%d %d \n", &str[0], &str[1]);
+	return 0;
+}

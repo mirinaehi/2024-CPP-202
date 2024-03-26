641a0c1a1ee41f12dc09b15e84534cf97815c489
mirinaehi <garook@e-mirim.hs.kr>
2024-03-26 16:10:09 +0900

    같은 결과2

--- 2024-CPP-202/grammar/배열과 포인터.cpp
+++ 2024-CPP-202/grammar/배열과 포인터.cpp
@@ -6,8 +6,7 @@ int main(void)
 	int arr[4] = { 10, 20, 30 ,40 };
 	char str[4] = "abc";
 
-	// 주소값 4바이트 만큼의 차이
-	printf("%d %d \n", &arr[0], &arr[1]);
-	printf("%d %d \n", arr+0, arr+1);
+	printf("%d %d \n", arr[0], arr[1]);
+	printf("%d %d \n", *(arr+0), *(arr+1));
 	return 0;
 }

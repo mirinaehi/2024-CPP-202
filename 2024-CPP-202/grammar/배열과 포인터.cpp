c4a97851463d45c7ed38417b9fbf220654b0e21b
mirinaehi <garook@e-mirim.hs.kr>
2024-03-28 11:08:22 +0900

    포인터 연산

--- 2024-CPP-202/grammar/배열과 포인터.cpp
+++ 2024-CPP-202/grammar/배열과 포인터.cpp
@@ -8,5 +8,19 @@ int main(void)
 
 	printf("%d %d \n", arr[0], arr[1]);
 	printf("%d %d \n", *(arr+0), *(arr+1));
+
+	int i = 1;
+	char c = 'b';
+	double d = 3.0;
+	
+	int* pi = &i;
+	char* pc = &c;
+	double* pd = &d;
+
+	// 각각 4, 1, 8 차이
+	printf("%d %d %d \n", pi, pc, pd);
+	printf("%d %d %d \n", pi+1, pc+1, pd+1);
+
+
 	return 0;
 }

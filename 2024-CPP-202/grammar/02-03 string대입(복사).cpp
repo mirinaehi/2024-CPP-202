2c8a701b4a700394aca2b95ed77407ab82b1df5b
mirinaehi <garook@e-mirim.hs.kr>
2024-04-04 11:18:42 +0900

    string 대입1(C언어 방식)

---
+++ 2024-CPP-202/grammar/02-03 string대입(복사).cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	char str[30] = "develop hungry";
+	char str2[30];
+	
+	// str에 있는 내용을 str2에 복사
+	// '\0'을 만날때까지
+	int i = 0;
+	for (; str[i] != '\0'; i++) {
+		str2[i] = str[i];
+	}
+	str2[i] = str[i];	// 널문자('\0') 복사
+
+	printf("%s ", str2);
+
+
+
+}

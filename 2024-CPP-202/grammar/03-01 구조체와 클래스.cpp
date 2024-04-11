c2f7803428f120e434fe001de2660ae251459375
mirinaehi <garook@e-mirim.hs.kr>
2024-04-11 12:09:46 +0900

    객체 배열 동적할당 해제

--- 2024-CPP-202/grammar/03-01 구조체와 클래스.cpp
+++ 2024-CPP-202/grammar/03-01 구조체와 클래스.cpp
@@ -34,16 +34,17 @@ private:
 int main(void)
 {
 	// 동적할당 : 메모리는 heap에서 할당. 실행(런타임) 시 메모리 크기가 정해짐
-	Student* jisu = new Student(2202, "나지수");
-	
-	// 정적할당 : 메모리는 stack에서 할당. 컴파일 시 메모리 크기가 정해짐
-	Student swag = Student();
+	Student* stu = new Student[3]{
+		{2202, "나지수"},
+		{2203, "박지영"},
+		{2215, "choo"}
+	};
 
-	jisu->show();
-	swag.show();
+	for(int i=0; i<3; i++)
+		stu[i].show();
 
-	// 동적할당 해제
-	delete jisu;
+	// 배열의 동적할당 해제
+	delete []stu;
 
 	return 0;
 }

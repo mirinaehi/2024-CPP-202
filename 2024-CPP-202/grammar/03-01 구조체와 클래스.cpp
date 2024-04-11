284bb6b044c1a06d631c1592cc57137ffb0df8a0
mirinaehi <garook@e-mirim.hs.kr>
2024-04-11 11:04:55 +0900

    03-01 멤버함수 만들기

--- 2024-CPP-202/grammar/03-01 구조체와 클래스.cpp
+++ 2024-CPP-202/grammar/03-01 구조체와 클래스.cpp
@@ -17,6 +17,14 @@ public:
 		hakbun_ = hakbun;
 		name_ = name;
 	}
+
+	// 클래스의 멤버를 출력
+	void show(void)
+	{
+		cout << hakbun_ << " ";
+		cout << name_ << endl;
+	}
+
 private:
 	int hakbun_;
 	string name_;
@@ -26,6 +34,9 @@ int main(void)
 {
 	Student jisu = Student(2202, "나지수");
 	Student swag = Student();
+
+	jisu.show();
+	swag.show();
 
 	return 0;
 }

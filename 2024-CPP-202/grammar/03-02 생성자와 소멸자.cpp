783474ca71b6705e0fd6b6fa548c6801e4d9c81d
mirinaehi <garook@e-mirim.hs.kr>
2024-04-18 11:15:58 +0900

    03-02 소멸자

--- 2024-CPP-202/grammar/03-02 생성자와 소멸자.cpp
+++ 2024-CPP-202/grammar/03-02 생성자와 소멸자.cpp
@@ -7,15 +7,19 @@ using namespace std;
 class Student {
 public:
 
-	// TODO : 생성자에서 동적할당을 한 메모리 공간을 지울 수 없다
 	Student(int hakbun, const char* name) : hakbun_(hakbun)
 	{
 		int length = strlen(name);
 
 		// '/0'가 들어가기 위해 공간 하나 더 추가
 		name_ = new char[length+1];
-		
 		strcpy(name_, name);
+	}
+
+	// 소멸자 : 객체가 소멸될 때(메모리에서 지워질 때) 호출되는 함수
+	~Student(void)
+	{
+		delete []name_;
 	}
 
 	// 클래스의 멤버를 출력
@@ -33,8 +37,6 @@ private:
 int main(void)
 {
 	Student* stu = new Student(2213, "조수빈");
-	stu->show();
-
 	delete stu;
 
 	return 0;

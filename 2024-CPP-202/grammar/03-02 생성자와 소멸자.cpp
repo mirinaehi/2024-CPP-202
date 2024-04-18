8553be86f75dde4fcba1e51f122caaf302f88b20
mirinaehi <garook@e-mirim.hs.kr>
2024-04-18 11:45:03 +0900

    03-02 생성자, 소멸자 호출시기 확인

--- 2024-CPP-202/grammar/03-02 생성자와 소멸자.cpp
+++ 2024-CPP-202/grammar/03-02 생성자와 소멸자.cpp
@@ -14,12 +14,14 @@ public:
 		// '/0'가 들어가기 위해 공간 하나 더 추가
 		name_ = new char[length+1];
 		strcpy(name_, name);
+		cout << "생성자 호출 완료" << endl;
 	}
 
 	// 소멸자 : 객체가 소멸될 때(메모리에서 지워질 때) 호출되는 함수
 	~Student(void)
 	{
 		delete []name_;
+		cout << "소멸자 호출 완료" << endl;
 	}
 
 	// 클래스의 멤버를 출력
@@ -37,7 +39,10 @@ private:
 int main(void)
 {
 	Student* stu = new Student(2213, "조수빈");
+	cout << "I'm still hungry - 거스 히딩크" << endl;
+
 	delete stu;
+	cout << "It ain't over till it's over - 요기 베라" << endl;
 
 	return 0;
 }

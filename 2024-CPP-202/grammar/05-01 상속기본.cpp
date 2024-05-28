4f20f224aa4e9337620dd3905545d6aa89c209c3
mirinaehi <garook@e-mirim.hs.kr>
2024-05-28 16:08:10 +0900

    05-01 부모/자식 생성/소멸자 호출순서

--- 2024-CPP-202/grammar/05-01 상속기본.cpp
+++ 2024-CPP-202/grammar/05-01 상속기본.cpp
@@ -5,7 +5,14 @@ using namespace std;
 class Person {
 public:
 	Person(string name, int age)
-		: name_(name), age_(age) {}
+		: name_(name), age_(age) 
+	{
+		cout << "부모생성자 호출" << endl;
+	}
+	~Person()
+	{
+		cout << "부모소멸자 호출" << endl;
+	}
 
 	void show_person() {
 		cout << name_ << "의 나이는 " << age_ << "입니다." << endl;
@@ -21,7 +28,11 @@ public:
 	// 부모생성자가 먼저 호출
 	Student(string name, int age, int id) : Person(name, age), id_(id)
 	{
-
+		cout << "자식생성자 호출" << endl;
+	}
+	~Student()
+	{
+		cout << "자식소멸자 호출" << endl;
 	}
 	void show_person()
 	{
@@ -36,5 +47,6 @@ int main(void)
 {
 	Student* stu = new Student("조드리헵번", 39, 1004);
 	stu->show_person();
+	delete stu;
 	return 0;
 }

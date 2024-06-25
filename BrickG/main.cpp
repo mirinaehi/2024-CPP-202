d42c932759342483b85e168e35f3608910d985e0
mirinaehi <garook@e-mirim.hs.kr>
2024-06-25 14:55:54 +0900

    [bricks] 기본예제

---
+++ BrickG/main.cpp
@@ -0,0 +1,42 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // 창 생성
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Shapes");
+    window.setFramerateLimit(60);
+
+    // 원 객체 생성
+    sf::CircleShape circle(100.f);
+    circle.setFillColor(sf::Color::Red);
+    circle.setPosition(0.f, 0.f);
+
+    // 사각형 객체 생성
+    sf::RectangleShape rectangle(sf::Vector2f(200.f, 100.f));
+    rectangle.setFillColor(sf::Color::Blue);
+    rectangle.setPosition(0.f, 0.f);
+
+    // 이벤트 루프 시작
+    while (window.isOpen())
+    {
+        rectangle.move(0.5f, 0);
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // 화면 지우기
+        window.clear();
+
+        // 도형 그리기
+        window.draw(circle);
+        window.draw(rectangle);
+
+        // 화면 업데이트
+        window.display();
+    }
+
+    return 0;
+}

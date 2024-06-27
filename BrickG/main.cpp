591d8baf94de0e6b47de2622aceddbdaf67eee2a
mirinaehi <garook@e-mirim.hs.kr>
2024-06-27 11:55:54 +0900

    paddle 추가

--- BrickG/main.cpp
+++ BrickG/main.cpp
@@ -30,12 +30,46 @@ public:
     float bottom() { return shape.getPosition().y + shape.getRadius(); }
 };
 
+// Paddle 클래스 정의
+class Paddle {
+public:
+    sf::RectangleShape shape;
+    const float paddleWidth = 100.f;
+    const float paddleHeight = 20.f;
+    const float paddleVelocity = 10.f;
+
+    Paddle(float mX, float mY) {
+        shape.setPosition(mX, mY);
+        shape.setSize({ paddleWidth, paddleHeight });
+        shape.setFillColor(sf::Color::Blue);
+        // 기준점을 중심으로
+        shape.setOrigin(paddleWidth / 2.f, paddleHeight / 2.f);
+    }
+
+    void update() {
+        // 왼쪽 화살키를 누르고 && 왼쪽 끝에 도달하지 않은 경우
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) && left() > 0) {
+            shape.move(-paddleVelocity, 0.f);
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && right() < 800) {
+            shape.move(paddleVelocity, 0.f);
+        }
+    }
+
+    float left() { return shape.getPosition().x - shape.getSize().x / 2.f; }
+    float right() { return shape.getPosition().x + shape.getSize().x / 2.f; }
+    // 지금 당장은 쓸모가 없지만, 확장을 고려해서 일단 놔두겠다.
+    float top() { return shape.getPosition().y - shape.getSize().y / 2.f; }
+    float bottom() { return shape.getPosition().y + shape.getSize().y / 2.f; }
+};
+
 int main()
 {
     // 창 생성
     sf::RenderWindow window(sf::VideoMode(800, 600), "bricks");
     window.setFramerateLimit(60);   // 초당 프레임을 60으로
-
+    
+    Paddle paddle(600.f, 550.f);
 
     // 이벤트 루프 시작
     while (window.isOpen())
@@ -48,10 +82,15 @@ int main()
                 window.close();
         }
 
+        // update
+        paddle.update();
+
+        // draw
         // 화면 지우기
         window.clear(sf::Color::White);
 
         // 그리기
+        window.draw(paddle.shape);
 
         // 화면 업데이트
         window.display();

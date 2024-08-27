3c16f2d6fa3661c0b0f28313dc51c4cf6445336e
JWPmaster <garook@e-mirim.hs.kr>
2024-08-27 16:05:10 +0900

    07-01 템플릿 적용 후

--- 2024-CPP-202/grammar/07-01 템플릿.cpp
+++ 2024-CPP-202/grammar/07-01 템플릿.cpp
@@ -3,24 +3,16 @@
 
 using namespace std;
 
-int sum(int a, int b)
+template <typename T>
+T sum(T a, T b)
 {
 	return a + b;
 }
 
-float sum(float a, float b)
-{
-	return a + b;
-}
-
-string sum(string a, string b)
-{
-	return a + b;
-}
 
 void main(void)
 {
-	cout << sum(1, 2) << endl;
-	cout << sum(1.f, 2.f) << endl;
-	cout << sum("1", "2") << endl;
+	cout << sum<int>(1, 2) << endl;
+	cout << sum<float>(1.f, 2.f) << endl;
+	cout << sum<string>("1", "2") << endl;
 }

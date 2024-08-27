fa1b6dd4099914a407dfe6189328dd31fb031f6a
JWPmaster <garook@e-mirim.hs.kr>
2024-08-27 14:52:31 +0900

    순수 가상함수

--- 2024-CPP-202/grammar/05-03 다형성.cpp
+++ 2024-CPP-202/grammar/05-03 다형성.cpp
@@ -18,20 +18,10 @@ public:
 		cout << "Animal 소멸자" << endl;
 	}
 
-	virtual void walk(void)
-	{
-		cout << "걷다" << endl;
-	}
-
-	virtual void bark(void)
-	{
-		cout << "짖다" << endl;
-	}
-
-	virtual void eat(void)
-	{
-		cout << "먹다" << endl;
-	}
+	// 순수 가상함수(추상메서드)
+	virtual void walk(void) = 0;
+	virtual void bark(void) = 0;
+	virtual void eat(void) = 0;
 private:
 	string name_;
 	unsigned int age_;
@@ -60,8 +50,9 @@ private:
 
 void main(void)
 {
+	// 추상클래스는 객체를 생성할 수 없다(new Animal() 불가)
 	Animal* animal = new Dog("마루", 5, 2, 100);
-	
+	animal->bark();
 	delete animal;
 
 }

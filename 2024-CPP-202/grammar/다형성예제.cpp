c5c6e2c2e6421ef910cbd3f2f3397a154dbf4f76
JWPmaster <garook@e-mirim.hs.kr>
2024-09-03 13:57:15 +0900

    다형성 적용 전

--- 2024-CPP-202/grammar/다형성예제.cpp
+++ 2024-CPP-202/grammar/다형성예제.cpp
@@ -7,7 +7,16 @@ class Food {
 public:
 	Food(int civil, int force, string name, int territory)
 		: civil_(civil), force_(force), name_(name), territory_(territory)
-	{}
+	{
+	}
+
+	void show()
+	{
+		cout << "국민 수 : " << civil_ << endl;
+		cout << "군력 : " << force_ << endl;
+		cout << "이름 : " << name_ << endl;
+		cout << "면적 : " << territory_ << endl;
+	}
 
 private:
 	int civil_;		// 국민 수
@@ -21,6 +30,12 @@ public:
 	Kimchi(int civil, int force, string name, int territory, int garlic, int pepper)
 		: Food(civil, force, name, territory), garlic_(garlic), pepper_(pepper)
 	{}
+
+	void show()
+	{
+		cout << "마늘 : " << garlic_ << endl;
+		cout << "고추 : " << pepper_ << endl;
+	}
 private:
 	int garlic_;
 	int pepper_;		// 한글의 힘
@@ -31,6 +46,12 @@ public:
 	Jelly(int civil, int force, string name, int territory, int gelatin, int sugar)
 		: Food(civil, force, name, territory), gelatin_(gelatin), sugar_(sugar)
 	{}
+
+	void show()
+	{
+		cout << "젤라틴 : " << gelatin_ << endl;
+		cout << "설탕 : " << sugar_ << endl;
+	}
 private:
 	int gelatin_;
 	int sugar_;		// 설탕의 힘
@@ -38,10 +59,30 @@ private:
 
 class Cheese : public Food {
 public:
-	Cheese(int civil, int force, string name, int territory, int milk, int Rennet)
-		: Food(civil, force, name, territory), milk_(milk), Rennet_(Rennet)
+	Cheese(int civil, int force, string name, int territory, int milk, int rennet)
+		: Food(civil, force, name, territory), milk_(milk), rennet_(rennet)
 	{}
+
+	void show()
+	{
+		cout << "우유 : " << milk_ << endl;
+		cout << "응고제 : " << rennet_ << endl;
+	}
 private:
 	int milk_;
-	int Rennet_;	// 응고제
+	int rennet_;	// 응고제
 };
+
+
+void main(void)
+{
+	Food* player = new Kimchi(15, 95, "갓김치", 86, 100, 100);
+	Food* friends = new Cheese(100, 20, "짜계치", 100, 20, 100);
+
+	player->show();
+	cout << endl << endl;
+	friends->show();
+
+	delete friends;
+	delete player;
+}

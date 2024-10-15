dc168781697e80aaa938ef7f8a3dcd6c05e270fd
JWPmaster <garook@e-mirim.hs.kr>
2024-10-15 14:20:23 +0900

    09-02 전통적인 반복문

---
+++ 2024-CPP-202/grammar/09-02 범위기반 for문.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void main(void)
+{
+	vector<int> v = { 445, 14, 27, 456, 23 };
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
+
+
+}

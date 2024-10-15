64efbbfd0723a4a045e7187da52b9936f7001969
JWPmaster <garook@e-mirim.hs.kr>
2024-10-15 14:39:00 +0900

    09-03 auto

---
+++ 2024-CPP-202/grammar/09-03 auto.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void main(void)
+{
+	vector<string> v = { "롯데관광", "동화면세점", "제주드림타워", "마이데일리" };
+
+	for (auto str : v)
+		cout << str << endl;
+
+
+}
